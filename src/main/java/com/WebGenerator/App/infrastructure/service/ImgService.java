a6952cb8388e799e38a1de81047c3d350179b642
Felipe <macedoflp@hotmail.com>
2025-04-13 11:18:25 -0300

    fix: ImgService file

--- src/main/java/com/WebGenerator/App/infrastructure/service/ImgService.java
+++ src/main/java/com/WebGenerator/App/infrastructure/service/ImgService.java
@@ -31,16 +31,10 @@ public class ImgService {
             byte[] fileBytes = file.getBytes();
             String base64File = Base64.getEncoder().encodeToString(fileBytes);
 
-            String contentType = file.getContentType();
-            String base64WithPrefix = "data:" + contentType + ";base64," + base64File;
-
-            FileCreateRequest request = new FileCreateRequest(base64WithPrefix, file.getOriginalFilename());
+            FileCreateRequest request = new FileCreateRequest(base64File, file.getOriginalFilename());
             request.setPrivateFile(false);
 
             Result result = imageKit.upload(request);
-            if (result == null) {
-                throw new RuntimeException("Upload falhou.");
-            }
 
             List<Map<String, String>> transformation = new ArrayList<>();
 
@@ -49,9 +43,6 @@ public class ImgService {
             options.put("path", result.getFilePath());
             options.put("transformation", transformation);
 
-            if (result.getUrl() == null) {
-                throw new RuntimeException("Upload falhou ou URL da imagem não retornada.");
-            }
             return result.getUrl();
 
         } catch (IOException e) {
