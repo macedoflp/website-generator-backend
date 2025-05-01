6b0da27168a0ebf3a8d0c4b88a5faa0c744f9043
Felipe Macedo <141750211+macedoflp@users.noreply.github.com>
2025-05-01 18:17:27 -0300

    refact paymentcontroller

--- src/main/java/com/WebGenerator/App/api/controller/PaymentController.java
+++ src/main/java/com/WebGenerator/App/api/controller/PaymentController.java
@@ -35,7 +35,6 @@ public class PaymentController {
         SessionCreateParams params = SessionCreateParams.builder()
                 .setUiMode(SessionCreateParams.UiMode.CUSTOM)
                 .setMode(SessionCreateParams.Mode.PAYMENT)
-                .setReturnUrl(domain + "/return?session_id={CHECKOUT_SESSION_ID}")
                 .addLineItem(SessionCreateParams.LineItem.builder()
                         .setQuantity(1L)
                         .setPrice(priceId)
